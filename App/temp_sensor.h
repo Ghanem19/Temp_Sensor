6fbef49359d87819dd3b45542bc606046b65e212
Ghanem19 <45525556+Ghanem19@users.noreply.github.com>
2020-01-27 16:24:58 +0200

    Update temp_sensor.h

--- App/temp_sensor.h
+++ App/temp_sensor.h
@@ -2,12 +2,50 @@
  * temp_sensor.h
  *
  *  Created on: Jan 26, 2020
- *      Author: eng-m
+ *      Author: Ghanem
  */
 
-#ifndef APP_TEMP_SENSOR_H_
-#define APP_TEMP_SENSOR_H_
+#ifndef APP_TEMPSENSOR_H_
+#define APP_TEMPSENSOR_H_
+/************************************************************************/
+/*		                        Includes                 		        */
+/************************************************************************/
+#include "std_types.h"
+
+
+/************************************************************************/
+/*		             External Functions  PROTOTYPES       		        */
+/************************************************************************/
+
+/*
+ * Function: TempSensor_init.
+ *
+ * Return: ERROR_Status each error code represent certain error to ease debugging
+ *
+ * Description: initialize the ADC module to interface with LM34/35 sensor.
+ */
+extern ERR_STATUS	TempSensor_init(void);
+
+/*
+ * Function: TempSensor_convTrigger.
+ *
+ * Return: ERROR_Status each error code represent certain error to ease debugging
+ *
+ * Description: trigger the ADC to start converting the analog data.
+ */
+extern ERR_STATUS	TempSensor_convTrigger(void);
+
+/*
+ * Function: TempSensor_readValue.
+ *
+ * OUTPUTS: *u8_tempValue -> data returned to user by that pointer
+ *
+ * Return: ERROR_Status each error code represent certain error to ease debugging
+ *
+ * Description: get the converted digital value when the conversion complete .
+ */
+extern ERR_STATUS	TempSensor_readValue(uint16 *u8_tempValue);
 
 
 
-#endif /* APP_TEMP_SENSOR_H_ */
+#endif /* APP_TEMPSENSOR_H_ */
